fc2185e99d0ff2ddbfe84cd8c8e995a69a928b51
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2025-06-15 22:02:49 -0300

    chore: remove debug

--- src/main.ts
+++ src/main.ts
@@ -35,6 +35,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, docConfig);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, () =>
+    console.log(`Rodando na porta ${process.env.PORT ?? 3000}`)
+  );
 }
 bootstrap();
